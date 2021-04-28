8f60c45bfe3ad80634b1f3b2be6d00f6cd2165e9
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-04-28 16:55:51 +0200

    Add function that add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
 function titleClickHandler(event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
 
 
-    /* remove class 'active' from all article links */
+    /* [DONE] remove class 'active' from all article links */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,9 +14,13 @@ function titleClickHandler(event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post.active');
 
