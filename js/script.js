9882c6f56231f187a954abb5ac152d37b19e67e8
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-04-28 22:05:04 +0200

    Add function that finds the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 'use strict';
 
 function titleClickHandler(event) {
+
+    event.preventDefault();
+
     const clickedElement = this;
+
     console.log('Link was clicked!');
     console.log(event);
+
 
 
     /* [DONE] remove class 'active' from all article links */
@@ -14,7 +19,7 @@ function titleClickHandler(event) {
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
 
@@ -30,7 +35,13 @@ function titleClickHandler(event) {
 
     /* get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
 
